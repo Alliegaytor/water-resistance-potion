96f645226d6763471a686638ac53f71eef64490e
Allison Smith <allisondaniellesmith@protonmail.com>
2023-07-15 13:39:13 +1000

    Hotfix: Change java target version to 16
    - Change java target from 17 to 16
    - Use my own mixin plugin over conditional-mixin
    - Fix fabric-api dependency

---
+++ src/main/java/xyz/alycat/hwr/mixinplugin/MixinConfigPlugin.java
@@ -0,0 +1,66 @@
+package xyz.alycat.hwr.mixinplugin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import xyz.alycat.hwr.Hwr;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Boilerplate, yay :3
+ * Why do I even need to write this?!??!
+ */
+public class MixinConfigPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        try {
+            Class<?> mixinClass = Class.forName(mixinClassName);
+            if (mixinClass.isAnnotationPresent(Restriction.class)) {
+                Condition[] conditions = mixinClass.getAnnotation(Restriction.class).require();
+                for (Condition condition : conditions) {
+                    if (!isModLoaded(condition.value())) {
+                        Hwr.LOGGER.info("Mod '" + condition.value() + "' is not present, skipping mixin: " + mixinClassName);
+                        return false;
+                    }
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    // Helper method to check if a mod is loaded
+    private static boolean isModLoaded(String modId) {
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+}
