25b198fab6589959a5e0d5c9c734b268b746ddf1
Allison Smith <allisondaniellesmith@protonmail.com>
2023-07-16 13:58:35 +1000

    Cleanup LivingEntityMixinOrigins

--- src/main/java/xyz/alycat/hwr/mixin/LivingEntityMixinOrigins.java
+++ src/main/java/xyz/alycat/hwr/mixin/LivingEntityMixinOrigins.java
@@ -20,22 +20,11 @@ public abstract class LivingEntityMixinOrigins {
      * Negates player water damage if player has WATER_RESISTANCE status effect.
      * Currently, only works with the Origins hydrophobic classes
      */
-    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
-    private void injectDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cfr) {
-        // Cast the entity to LivingEntity
+    @Inject(method = "damage", at = @At(value = "HEAD", target = "Lnet/minecraft/entity/LivingEntity;isSleeping()Z", ordinal = 0), cancellable = true)
+    private void injectDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity entity = (LivingEntity) (Object) this;
-
-        if (entity.hasStatusEffect(ModStatusEffects.WATER_RESISTANCE)) {
-            // Reduce CPU overhead by checking if player first. Most entities are not players.
-            if (entity.isPlayer()) {
-                if (!entity.isInvulnerableTo(source)) {
-                    if (source.getName().equals("hurt_by_water")) {
-                        // Negate damage to entity
-                        cfr.setReturnValue(false);
-                        cfr.cancel();
-                    }
-                }
-            }
+        if (source.getName().equals("hurt_by_water") && entity.hasStatusEffect(ModStatusEffects.WATER_RESISTANCE)) {
+            cir.setReturnValue(false);
         }
     }
 }
