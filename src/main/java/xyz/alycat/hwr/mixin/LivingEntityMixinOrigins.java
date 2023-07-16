655e6371e5cf98a51816ed49c23e36f2dc09f453
Allison Smith <allisondaniellesmith@protonmail.com>
2023-07-16 13:58:35 +1000

    Cleanup LivingEntityMixinOrigins

--- src/main/java/xyz/alycat/hwr/mixin/LivingEntityMixinOrigins.java
+++ src/main/java/xyz/alycat/hwr/mixin/LivingEntityMixinOrigins.java
@@ -4,7 +4,6 @@ import xyz.alycat.hwr.mixinplugin.Condition;
 import xyz.alycat.hwr.mixinplugin.Restriction;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,22 +20,11 @@ public abstract class LivingEntityMixinOrigins {
      * Negates player water damage if player has WATER_RESISTANCE status effect.
      * Currently, only works with the Origins hydrophobic classes
      */
-    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
-    private void injectDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cfr) {
-        // Cast the entity to LivingEntity
+    @Inject(method = "damage", at = @At(value = "HEAD", target = "Lnet/minecraft/entity/LivingEntity;isSleeping()Z", ordinal = 0), cancellable = true)
+    private void injectDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity entity = (LivingEntity) (Object) this;
-
-        if (entity.hasStatusEffect(ModStatusEffects.WATER_RESISTANCE)) {
-            // Reduce CPU overhead by checking if player first. Most entities are not players.
-            if (entity instanceof PlayerEntity) {
-                if (!entity.isInvulnerableTo(source)) {
-                    if (source.getName().equals("hurt_by_water")) {
-                        // Negate damage to entity
-                        cfr.setReturnValue(false);
-                        cfr.cancel();
-                    }
-                }
-            }
+        if (source.getName().equals("hurt_by_water") && entity.hasStatusEffect(ModStatusEffects.WATER_RESISTANCE)) {
+            cir.setReturnValue(false);
         }
     }
 }
