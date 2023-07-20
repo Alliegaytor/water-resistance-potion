fb09bd965fd67d71ca2acfd8d54facd3b62abaea
Allison Smith <allisondaniellesmith@protonmail.com>
2023-07-21 06:28:25 +1000

    Enchanted golden apples now give water resistance effect

--- src/main/java/xyz/alycat/hwr/potion/ModPotions.java
+++ src/main/java/xyz/alycat/hwr/potion/ModPotions.java
@@ -1,6 +1,5 @@
 package xyz.alycat.hwr.potion;
 
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
@@ -9,20 +8,20 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import xyz.alycat.hwr.Hwr;
-import xyz.alycat.hwr.effect.ModStatusEffects;
+import xyz.alycat.hwr.effect.ModStatusEffectInstance;
 import xyz.alycat.hwr.mixin.BrewingRecipeRegistryMixin;
 
 public class ModPotions {
     public static Potion WATER_RESISTANCE;
     public static Potion LONG_WATER_RESISTANCE;
 
-    public static Potion registerPotion(String name, StatusEffect effect, int duration, int amplifier) {
-        return Registry.register(Registries.POTION, new Identifier(Hwr.MOD_ID, name), new Potion(new StatusEffectInstance(effect, duration, amplifier)));
+    public static Potion registerPotion(String name, StatusEffectInstance statusEffectInstance) {
+        return Registry.register(Registries.POTION, new Identifier(Hwr.MOD_ID, name), new Potion(statusEffectInstance));
     }
 
     public static void registerPotions() {
-        WATER_RESISTANCE = registerPotion("water_resistance_potion", ModStatusEffects.WATER_RESISTANCE, 3600, 0);
-        LONG_WATER_RESISTANCE = registerPotion("long_water_resistance_potion", ModStatusEffects.WATER_RESISTANCE, 9600, 0);
+        WATER_RESISTANCE = registerPotion("water_resistance_potion", ModStatusEffectInstance.WATER_RESISTANCE_EFFECT);
+        LONG_WATER_RESISTANCE = registerPotion("long_water_resistance_potion", ModStatusEffectInstance.WATER_RESISTANCE_EFFECT_LONG);
         registerPotionRecipes();
     }
 
