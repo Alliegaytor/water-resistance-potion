c73e75f597178d6f56dd02171107e8b2ba468d76
Allison Smith <allisondaniellesmith@protonmail.com>
2024-02-18 22:07:20 +1100

    Remove uneeded config sync
    - Client does not need to share the same recipe config as server
    
    - Better document config options

--- src/main/java/xyz/alycat/hwr/config/HwrConfigModel.java
+++ src/main/java/xyz/alycat/hwr/config/HwrConfigModel.java
@@ -1,19 +1,22 @@
 package xyz.alycat.hwr.config;
 
-import io.wispforest.owo.config.Option;
+//import io.wispforest.owo.config.Option;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 import io.wispforest.owo.config.annotation.RestartRequired;
-import io.wispforest.owo.config.annotation.Sync;
+//import io.wispforest.owo.config.annotation.Sync;
 import xyz.alycat.hwr.Hwr;
 
 @Modmenu(modId = Hwr.MOD_ID)
 @Config(name = Hwr.MOD_ID, wrapperName = "ModConfig")
 public class HwrConfigModel {
+    // Client side
+    // Server handles the recipe config, no need to sync
     @RestartRequired
-    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public String potion_recipe = "minecraft:sponge";
 
+    // Client side
+    // Purely cosmetic change
     @RestartRequired
     public Colours potion_colour = Colours.purple;
 
